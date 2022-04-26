8bba07f37fa86faf7f23c1066c9a3cd445f7c76b
Laise2609 <85645803+Laise2609@users.noreply.github.com>
2022-04-26 15:51:05 -0300

    exercício

---
+++ exercicio.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício - Laise</title>
+</head>
+<body> 
+ <!-- hoje é dia 26/04/2022, fulano tem 900 anos e nasceu em sp -->
+<?php
+    define ("DATA", "26/04/2022");
+    define ("NOME", "Jonas");
+    define ("IDADE", 18);
+    define ("CIDADE", "São Paulo");
+?>
+    <p>Hoje é dia <?=DATA?>. <?=NOME?> tem <?=IDADE?> anos de idade e nasceu em <?=CIDADE?> </p>
+
+</body>
+</html>
+
+
