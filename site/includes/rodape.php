5265fc1fcbe69fd72671fa1939d02717d2cb0990
Laise2609 <laise1326@gmail.com>
2022-05-04 16:53:44 -0300

    data e hora

--- site/includes/rodape.php
+++ site/includes/rodape.php
@@ -3,6 +3,11 @@
     <footer>
         <hr>
         <p>Site desenvolvido como exemplo</p>
+        <?php 
+        date_default_timezone_set("America/Sao_Paulo");
+        $data = date("d/m/Y H:i"); 
+        ?>
+        <p><?=$data?></p>
     </footer>
 </body>
 </html>
